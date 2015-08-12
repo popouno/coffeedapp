5bb6e249f78e7a346aee9a37554858fbea0f7dc1
Marialena Popo <Maria@Marialenas-MacBook-Pro.local>
2015-08-12 10:20:16 -0600

    ready to push

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'coffeedapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':[os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,6 +113,8 @@ TEMPLATE_DIRS = (
     os.path.join(MAIN_DIR, 'templates'),
     )
 
-STATICFILE_DIRS = (
+STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
 )
+
+STATIC_ROOT = 'staticfiles'
