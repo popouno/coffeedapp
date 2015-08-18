c5efe5ecdfbf7b756767c2e6a4be9c1341e4dde2
Marialena Popo <Maria@Marialenas-MacBook-Pro.local>
2015-08-18 09:24:26 -0600

    basic blog is working!

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 import core.views as coreviews
 
 urlpatterns = patterns('',
 	#Examples:
-	# url(r'^$', coffeedapp.vies.home', name='home'),
+	# url(r'^$', coffeedapp.views.home', name='home'),
 	# url(r'^blog/', include('blog.urls')),
 
 	url(r'^$', coreviews.LandingView.as_view()),
+	url(r'location/$', coreviews.LocationListView.as_view()),
+	url(r'location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name='location_list'),
 )
